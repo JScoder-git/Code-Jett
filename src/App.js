a06564ec06cbf143a094b33d024fa8fd269e8ded
Sahil <93818966+JScoder-git@users.noreply.github.com>
2024-04-10 22:40:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
 import EditorPage from './pages/EditorPage';
-import ParticleAnimation from '../src/components/ParticleAnimation';
+
 function App() {
     return (
         <>
